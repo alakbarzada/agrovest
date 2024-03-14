5914c25f510130e617e36b87a4b2a20c67b0f224
vusal <vusal0703@gmail.com>
2024-03-14 12:15:23 +0400

    yarisi hazir

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
-const deleteMenu = document.querySelector('.menu.delete-menu');
+const deleteMenu = document.querySelector('.menu .delete-menu');
 
 menuBtn.addEventListener('click',function() {
     menu.classList.add('active');
